6f75c20a3b54b345d2c469820b45dcce46a3a381
Marc Augier <m.augier@me.com>
2020-11-27 09:58:55 +0100

    Création d'un config-dist pour masquer le mot de passe MySQL

---
+++ inc/config-dist.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 
+ * Marc Augier
+ * (c) 2020
+ */
+
+// Installation du 'framework'
+$config['dossier_pages'] = "controleur/";
+$config['theme']         = "base";
+$config['accueil']       = "auteur";
+$config['erreur']       = "erreur-bdd";
+
+// dev pour avoir les messages d'information
+$config['mode']          = "prod"; 
+
+// Base de données
+$config['dbname']        = "blog";
+$config['user']          = "<utilisateur MySQL>";
+$config['pass']          = "<mot de passe MySQL>";
